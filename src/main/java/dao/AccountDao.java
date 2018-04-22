5fe5cd314a45c4ab84a59917203db0542bd604a9
qq1318944013 <36846733+qq1318944013@users.noreply.github.com>
2018-04-22 18:42:28 +0800

    更新了doc

--- src/main/java/dao/AccountDao.java
+++ src/main/java/dao/AccountDao.java
@@ -12,45 +12,36 @@ import entity.User;
 public interface AccountDao
 {
 	
-	
+	/**
+	 * 根据记录数判断用户名是否存在
+	 * @param username 传入一个用户名，用来判断用户名是否存在
+	 * @return 返回记录数，1是用户名已存在，0是不存在
+	 */
 	public int userNameIsExit(String username);
 	
     /**
      * 登录的方法，通过传入的用户名得到一个用户(user)对象
-     *
      * @param username 登录时传入的用户名
      * @return 返回一个用户（user）对象
      */
     public User getUserByUserName(String username);
 
     /**
-     * 创建一个新账户，赋予新账户相应的角色
-     *
-     * @param role 传入的角色id,根据该id赋予相应的角色
+     * 创建一个新用户
+     * @param user 传入一个用户类，用来创建一个新用户
+     * @return 返回影响的行数，如果是1则创建成功
      */
     public int createAccount(User user);
 
-    /**
-     * 设置员工（Staff）的信息
-     *
-     * @param identityNum 身份证
-     * @param resume      履历
-     * @param qq          QQ号
-     * @param telephone   电话号码
-     * @param email       邮箱
-     */
+   /**
+    * 设置员工信息
+    * @param staff 传入一个员工类，用来设置员工信息
+    */
     public void settingStaffInfo(Staff staff);
 
     /**
-     * 设置客户（customer）的信息
-     *
-     * @param identityNum 身份证
-     * @param school      学校
-     * @param gradeMajor  专业班级
-     * @param telephone   电话
-     * @param email       邮箱
-     * @param qq          QQ号
-     * @param classId     班级id
+     * 设置客户信息
+     * @param customer 传入一个客户类，用来设置客户信息
      */
     public void settingCustomerInfo(Customer customer);
 }
