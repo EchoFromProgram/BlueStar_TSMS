b9a37c8bddf9304cfa31ee02d8845651639f70ea
FishGoddess <1149062639@qq.com>
2018-04-25 00:20:55 +0800

    删除了一个导包错误

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -19,7 +19,7 @@ public class LoginController {
 	
 	@Resource(name="accountServiceImpl")
 	private AccountService accountService;
-	
+
 	@ResponseBody
 	@RequestMapping(path = "loginCheck.do", produces = {"application/json;charset=UTF8"})
 	public Object loginCheck(User user, HttpSession session)
@@ -45,14 +45,14 @@ public class LoginController {
 	public String login() {
 		return "Login";
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(path = "getSessionUser.do", produces = {"application/json;charset=UTF8"})
 	public Object getSessionUser(HttpSession session) {
 		// TODO html访问session， 
 		return session.getAttribute("user");
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(path = "getSessionHisPowers.do", produces = {"application/json;charset=UTF8"})
 	public Object getSessionHisPower(HttpSession session) {
