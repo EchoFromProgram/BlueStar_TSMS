b30a4374e75b06ab093b2ce52306c75a07a3ed15
qq1318944013 <36846733+qq1318944013@users.noreply.github.com>
2018-05-03 15:48:02 +0800

    更新了成绩相关的方法

--- src/main/java/entity/Clazz.java
+++ src/main/java/entity/Clazz.java
@@ -6,6 +6,8 @@ package entity;
  */
 public class Clazz {
 	
+	//班级id
+	private Integer classId;
 	
 	// 班级名
 	private String className;
@@ -30,9 +32,48 @@ public class Clazz {
 		this.courseId = courseId;
 	}
 
+
+	public Integer getClassId() {
+		return classId;
+	}
+
+	public void setClassId(Integer classId) {
+		this.classId = classId;
+	}
+
 	@Override
 	public String toString() {
-		return "Class [className=" + className + ", courseId=" + courseId + "]";
+		return "Clazz [classId=" + classId + ", className=" + className + ", courseId=" + courseId + "]";
 	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Clazz other = (Clazz) obj;
+		if (classId == null) {
+			if (other.classId != null)
+				return false;
+		} else if (!classId.equals(other.classId))
+			return false;
+		if (className == null) {
+			if (other.className != null)
+				return false;
+		} else if (!className.equals(other.className))
+			return false;
+		if (courseId == null) {
+			if (other.courseId != null)
+				return false;
+		} else if (!courseId.equals(other.courseId))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
