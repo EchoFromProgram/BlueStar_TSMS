99966785e69dcdd0767b430bd84656a58b58e3c2
FishGoddess <1149062639@qq.com>
2018-05-12 14:09:42 +0800

    创建用户方法完善

--- src/main/java/constant/Role.java
+++ src/main/java/constant/Role.java
@@ -8,11 +8,11 @@ package constant;
 public final class Role
 {
     // 老师是 1
-    public static final Integer TEACHER = 1;
+    public static final int TEACHER = 1;
 
     // 学生是 2
-    public static final Integer STUDENT = 2;
+    public static final int STUDENT = 2;
 
     // 管理员是 3
-    public static final Integer ADMIN = 3;
+    public static final int ADMIN = 3;
 }
--- src/main/java/constant/Type.java
+++ src/main/java/constant/Type.java
@@ -8,10 +8,10 @@ package constant;
 public final class Type
 {
     // 内部员工为 0
-    public static final Integer INNER_STAFF = 0;
+    public static final int INNER_STAFF = 0;
 
     // 外部人员为 1
-    public static final Integer OUTTER_CLIENT = 1;
+    public static final int OUTTER_CLIENT = 1;
     
 
 }
