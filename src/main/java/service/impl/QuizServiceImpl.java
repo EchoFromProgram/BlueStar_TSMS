9f55d6bdb08273b7c77c00a979b61bd4e2a3f838
FishGoddess <1149062639@qq.com>
2018-05-15 15:22:40 +0800

    对 Quiz 实体类进行了改造

--- src/main/java/service/impl/QuizServiceImpl.java
+++ src/main/java/service/impl/QuizServiceImpl.java
@@ -1,5 +1,9 @@
 package service.impl;
 
+import dao.NewQuizDao;
+import dto.AccountDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import service.QuizService;
 
 /**
@@ -8,7 +12,26 @@ import service.QuizService;
  * @author Fish
  * created by 2018-05-15 15:10
  */
+@Service
 public class QuizServiceImpl implements QuizService
 {
-    
+    private NewQuizDao quizDao = null;
+
+    @Autowired
+    public void setQuizDao(NewQuizDao quizDao)
+    {
+        this.quizDao = quizDao;
+    }
+
+    /**
+     * 主要是学生查看他填写的问卷，通过 userId 获得
+     *
+     * @param pageNumber 页数
+     * @param userId 用户 id
+     * @return 返回这个用户填写过的问卷
+     */
+    public AccountDto getQuizsByUser(Integer pageNumber, Integer userId)
+    {
+        return null;
+    }
 }
