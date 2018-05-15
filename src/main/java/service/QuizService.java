8385fc49eee03ed53aa08e74ba2b7512c2443d06
FishGoddess <1149062639@qq.com>
2018-05-15 20:47:38 +0800

    测试通过：getQuizsByUser

--- src/main/java/service/QuizService.java
+++ src/main/java/service/QuizService.java
@@ -1,5 +1,6 @@
 package service;
 
+import dto.AccountDto;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,5 +12,12 @@ import org.springframework.stereotype.Service;
 @Service
 public interface QuizService
 {
-
+    /**
+     * 主要是学生查看他填写的问卷，通过 userId 获得
+     *
+     * @param pageNumber 页数
+     * @param userId     用户 id
+     * @return 返回这个用户填写过的问卷
+     */
+    public AccountDto getQuizsByUser(Integer pageNumber, Integer userId, Integer courseId);
 }
--- src/main/java/service/impl/QuizServiceImpl.java
+++ src/main/java/service/impl/QuizServiceImpl.java
@@ -2,9 +2,14 @@ package service.impl;
 
 import dao.NewQuizDao;
 import dto.AccountDto;
+import entity.Quiz;
+import enums.impl.Common;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import service.QuizService;
+import utils.PageUtil;
+
+import java.util.List;
 
 /**
  * 问卷业务实现类
@@ -28,10 +33,13 @@ public class QuizServiceImpl implements QuizService
      *
      * @param pageNumber 页数
      * @param userId 用户 id
+     * @param courseId 课程 id，如果不用可以传入 null
      * @return 返回这个用户填写过的问卷
      */
-    public AccountDto getQuizsByUser(Integer pageNumber, Integer userId)
+    public AccountDto getQuizsByUser(Integer pageNumber, Integer userId, Integer courseId)
     {
-        return null;
+        PageUtil.toPage(pageNumber);
+        List<Quiz> quizzes = quizDao.getQuizByUserIdOrCourseId(1, courseId);
+        return new AccountDto<>(PageUtil.pageInfo(quizzes), Common.SUCCESS);
     }
 }
