1a793c8ca27481c0d103b54de8b52df2ccab3cd4
FishGoddess <1149062639@qq.com>
2018-05-17 20:17:08 +0800

    测试通过：SignUtil 工具类

--- src/main/java/utils/SignUtil.java
+++ src/main/java/utils/SignUtil.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -8,38 +9,104 @@ import java.util.Date;
  * @author Fish
  * created by 2018-05-17 16:24
  */
-public class SignUtil
+public final class SignUtil
 {
     /**
-     * 这是一段时间，包括区间开始和结束
+     * 得到时间，只有小时和分钟
+     *
+     * @param hour   小时
+     * @param minute 分钟
+     * @return 返回时间
      */
-    public static final class SignTime // 签到专用时间类
+    private static Calendar getCalendar(int hour, int minute)
     {
-        private int hour;
-        private int minute;
-
-        public SignTime(int hour, int minute)
-        {
-            this.hour = hour;
-            this.minute = minute;
-        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2000, 0, 0, hour, minute, 0);
+        return calendar;
     }
 
+    /**
+     * 得到时间，只有小时和分钟
+     *
+     * @param date 时间
+     * @return 返回时间
+     */
+    private static Calendar getCalendar(Date date)
+    {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        return getCalendar(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
+    }
+
+    // 早上签到开始时间
+    private static final Calendar MORNING_SIGN_TIME = getCalendar(9, 0);
+
     // 早上开课时间
-    private static final SignTime MORNING_BEGIN_TIME = new SignTime(9, 30);
+    private static final Calendar MORNING_BEGIN_TIME = getCalendar(9, 30);
 
     // 早上结课时间
-    private static final SignTime MORNING_END_TIME = new SignTime(12, 0);
+    private static final Calendar MORNING_END_TIME = getCalendar(12, 0);
+
+    // 下午签到开始时间
+    private static final Calendar AFTERNOON_SIGN_TIME = getCalendar(13, 0);
 
     // 下午开课时间
-    private static final SignTime AFTERNOON_BEGIN_TIME = new SignTime(9, 30);
+    private static final Calendar AFTERNOON_BEGIN_TIME = getCalendar(13, 30);
 
     // 下午结课时间
-    private static final SignTime AFTERNOON_END_TIME = new SignTime(12, 0);
+    private static final Calendar AFTERNOON_END_TIME = getCalendar(16, 0);
+
+    // 晚上签到时间
+    private static final Calendar EVENING_SIGN_TIME = getCalendar(18, 30);
 
     // 晚上开课时间
-    private static final SignTime EVENING_BEGIN_TIME = new SignTime(9, 30);
+    private static final Calendar EVENING_BEGIN_TIME = getCalendar(19, 0);
 
     // 晚上结课时间
-    private static final SignTime EVENING_END_TIME = new SignTime(12, 0);
+    private static final Calendar EVENING_END_TIME = getCalendar(22, 0);
+
+    /**
+     * 判断是否是签到时间
+     *
+     * @param date 要被判断的时间
+     * @return true 在签到时间内 false 不在签到时间内
+     */
+    public static boolean isSignTime(Date date)
+    {
+        // 重新得到只有小时和分钟的时间
+        Calendar calendar = getCalendar(date);
+
+        // 判断是否在签到时间开始和上课时间开始之间
+        if ((calendar.after(MORNING_SIGN_TIME) && calendar.before(MORNING_BEGIN_TIME))
+                || (calendar.after(AFTERNOON_SIGN_TIME) && calendar.before(AFTERNOON_BEGIN_TIME))
+                || (calendar.after(EVENING_SIGN_TIME) && calendar.before(EVENING_BEGIN_TIME)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 判断是否是上课时间
+     *
+     * @param date 要被判断的时间
+     * @return true 在上课时间内 false 不在上课时间内
+     */
+    public static boolean isClassTime(Date date)
+    {
+        // 重新得到只有小时和分钟的时间
+        Calendar calendar = getCalendar(date);
+
+        // 判断是否在签到时间开始和上课时间开始之间
+        if ((calendar.after(MORNING_BEGIN_TIME) && calendar.before(MORNING_END_TIME))
+                || (calendar.after(AFTERNOON_BEGIN_TIME) && calendar.before(AFTERNOON_END_TIME))
+                || (calendar.after(EVENING_BEGIN_TIME) && calendar.before(EVENING_END_TIME)))
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
