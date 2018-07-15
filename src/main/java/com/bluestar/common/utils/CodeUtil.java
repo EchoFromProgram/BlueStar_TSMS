c12064f5d2f8254dbd1e32d51cf05d88936770b4
FishGoddess <1149062639@qq.com>
2018-07-15 12:20:38 +0800

    对密码进行 MD5 加密

--- src/main/java/com/bluestar/common/utils/CodeUtil.java
+++ src/main/java/com/bluestar/common/utils/CodeUtil.java
@@ -1,6 +1,7 @@
 package com.bluestar.common.utils;
 
 import org.springframework.util.DigestUtils;
+import org.springframework.util.StringUtils;
 
 /**
  * 编码工具类
@@ -9,7 +10,7 @@ import org.springframework.util.DigestUtils;
  * ------> 1149062639@qq.com
  * created by 2018-07-14 17:58
  */
-public class CodeUtil {
+public final class CodeUtil {
 
     /**
      * 获得 MD5 编码后的字符串
@@ -17,7 +18,11 @@ public class CodeUtil {
      * @param str 要加密的字符串
      * @return 返回加密后的字符串
      */
-    private static String getMD5(String str) {
+    public static String getMD5(String str) {
+        if (StringUtils.isEmpty(str)) {
+            return "";
+        }
+
         return DigestUtils.md5DigestAsHex(str.getBytes());
     }
 }
