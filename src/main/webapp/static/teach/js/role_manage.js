d9c483da77daa4b70ab6152a6c44f989c2bc171d
Mackyhuang <973151766@qq.com>
2018-07-15 12:31:10 +0800

    角色列表改造完成

--- src/main/webapp/static/teach/js/role_manage.js
+++ src/main/webapp/static/teach/js/role_manage.js
@@ -50,15 +50,41 @@ function build_table(data) {
         var roleName = $("<td></td>").append(item.role);
         var powerName = $("<td></td>");
         for(var i = 0; i < item.powerNames.length; i++){
-        	powerName.append(item.powerNames[i].powerName + " &nbsp; , &nbsp; ");
+        	powerName.append(item.powerNames[i].powerName + " &nbsp; &nbsp; &nbsp; ");
         }
+        var opera = $("<td></td>").append(
+            '<button class="delete-role-btn btn btn-danger pull-right" href="#panel-user-4" data-toggle="tab" name="'+ item.role +'" role="button">'+
+            '删除'+
+            '</button>'+
+            '<button class="update-role-btn btn btn-primary pull-right pre-update-button" href="#panel-user-2" data-toggle="tab" name="'+ item.role +'" role="button">'+
+            '修改'+
+            '</button>'
+        )
 
         //向一个tr中添加所有的td
-        $("<tr></tr>").append(roleName).append(powerName).appendTo("#role-table tbody");
+        $("<tr></tr>").append(roleName).append(powerName).append(opera).appendTo("#role-table tbody");
+        goUpdate();
+        goDelete();
     })
 }
 
+//页面转跳函数
+function goUpdate(){
+    var btns = $('.update-role-btn');
+    btns.bind('click', function () {
+        $('#update-rolename').val($(this).attr('name'));
+        checkRole();
+    });
+}
 
+//页面转跳函数
+function goDelete(){
+    var btns = $('.delete-role-btn');
+    btns.bind('click', function () {
+        $('#delete-role-input').val($(this).attr('name'));
+        $('#modal-role-delete-a').trigger('click');
+    });
+}
 
 //显示签到信息的总函数
 function getAllRole(){
@@ -70,7 +96,7 @@ $(function(){
 });
 
 //点击创建角色，在页面添加选择框，并且删除另一个页面的选择框
-$("#add-role").click(function (){
+$("#submit-add").click(function (){
 	$("#select-box-add").empty();
 	$("#select-box-add").append('<div class="ue-container" style="margin-bottom: 15px">'+
 	    '<select multiple="multiple" size="10" name="doublebox" class="demo">'+
@@ -109,7 +135,7 @@ $("#add-role").click(function (){
 })
 
 //点击修改角色，在页面添加选择框，并并且删除另一个页面的选择框
-$("#update-role").click(function (){
+$(".update-role-btn").click(function (){
 	$("#select-box-update").empty();
 	$("#select-box-update").append('<div class="ue-container" style="margin-bottom: 15px">'+
 	    '<select multiple="multiple" size="10" name="doublebox" class="demo">'+
@@ -164,79 +190,81 @@ $("#delete-role-button").click(function(){
 });
 
 //角色更新前的操作
-$("#update-rolename").mouseleave(function(){
-	var flag = 1;
-	var roleId;
-	var personPower = new Array();
-	inputId = $(this).val();
-	$.each(roleTable, function(index, item){	
-		if(item.role == inputId){
-			roleId = item.roleId;
-			$.each(item.powerNames, function(index, item){
-				personPower.push(item);
-			});
-			flag = 0;
-			return false;
-		}
-	});
-	
-	if(flag == 1){
-		$("#submit-update-role").attr("disabled", true); 
-		$("#rolename-change").addClass("has-error");
-		$("#rolename-change").removeClass("has-success");
-		$("#update-rolename-help").text("角色名不存在，请核对后再输入");
-	}else{
-		$("#submit-update-role").removeAttr("disabled");
-		$("#rolename-change").removeClass("has-error");
-		$("#rolename-change").addClass("has-success");
-		$("#submit-update-role").attr("update-prop", roleId);
-		$("#update-rolename-help").text("角色名输入正确，请操作其权限");
-		//获取权限表以及设置左右选择框
-		$.ajax({
-		    url:"getPowerTable.do",
-		    type:"POST",
-		    dataType:"json",
-		    success:function (data) {
-		    	$("#select-box-update").empty();
-		    	$("#select-box-update").append('<div class="ue-container" style="margin-bottom: 15px">'+
-		    	    '<select multiple="multiple" size="10" name="doublebox" class="demo">'+
-		    	    '</select>'+
-		    		'</div>'
-		    	);
-		        var nonSelect = new Array();
-		        $.each(data, function (index, item) { 
-		        	var exist = 1;
-		        	for(var i = 0; i < personPower.length; i++)
-		        	{
-		        		if(item.powerId == personPower[i].powerId)
-		        		{
-		        			exist = 0;
-		        			break;
-		        		}
-		        	}
-		        	if(exist == 1){
-		        		nonSelect.push(item);
-		        	}
-		        })
-		        var demo1 = $('.demo').doublebox({
-			        nonSelectedListLabel: '选择角色',
-			        selectedListLabel: '授权用户角色',
-			        preserveSelectionOnMove: 'moved',
-			        moveOnSelect: false,
-			        nonSelectedList:nonSelect,
-			        selectedList:personPower,
-			        optionValue:"powerId",
-			        optionText:"powerName",
-			        doubleMove:true,
-			    });
-		    },
-		    error:function () {
-		        alert("指定更新者的资料拉取失败");
-		    }
-		});
-		
-	}
-});
+$("#update-rolename").mouseleave(checkRole);
+
+function checkRole(){
+    var flag = 1;
+    var roleId;
+    var personPower = new Array();
+    inputId = $("#update-rolename").val();
+    $.each(roleTable, function(index, item){
+        if(item.role == inputId){
+            roleId = item.roleId;
+            $.each(item.powerNames, function(index, item){
+                personPower.push(item);
+            });
+            flag = 0;
+            return false;
+        }
+    });
+
+    if(flag == 1){
+        $("#submit-update-role").attr("disabled", true);
+        $("#rolename-change").addClass("has-error");
+        $("#rolename-change").removeClass("has-success");
+        $("#update-rolename-help").text("角色名不存在，请核对后再输入");
+    }else{
+        $("#submit-update-role").removeAttr("disabled");
+        $("#rolename-change").removeClass("has-error");
+        $("#rolename-change").addClass("has-success");
+        $("#submit-update-role").attr("update-prop", roleId);
+        $("#update-rolename-help").text("角色名输入正确，请操作其权限");
+        //获取权限表以及设置左右选择框
+        $.ajax({
+            url:"getPowerTable.do",
+            type:"POST",
+            dataType:"json",
+            success:function (data) {
+                $("#select-box-update").empty();
+                $("#select-box-update").append('<div class="ue-container" style="margin-bottom: 15px">'+
+                    '<select multiple="multiple" size="10" name="doublebox" class="demo">'+
+                    '</select>'+
+                    '</div>'
+                );
+                var nonSelect = new Array();
+                $.each(data, function (index, item) {
+                    var exist = 1;
+                    for(var i = 0; i < personPower.length; i++)
+                    {
+                        if(item.powerId == personPower[i].powerId)
+                        {
+                            exist = 0;
+                            break;
+                        }
+                    }
+                    if(exist == 1){
+                        nonSelect.push(item);
+                    }
+                })
+                var demo1 = $('.demo').doublebox({
+                    nonSelectedListLabel: '选择角色',
+                    selectedListLabel: '授权用户角色',
+                    preserveSelectionOnMove: 'moved',
+                    moveOnSelect: false,
+                    nonSelectedList:nonSelect,
+                    selectedList:personPower,
+                    optionValue:"powerId",
+                    optionText:"powerName",
+                    doubleMove:true,
+                });
+            },
+            error:function () {
+                alert("指定更新者的资料拉取失败");
+            }
+        });
+
+    }
+}
 
 //点击修改角色
 $("#submit-update-role").click(updateRole);
